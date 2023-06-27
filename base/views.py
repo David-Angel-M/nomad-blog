e5286debdbac7fc9d1008070fcdb498a0e797ea6
David-Angel-M <110673903+David-Angel-M@users.noreply.github.com>
2023-06-27 12:52:35 -0500

    Login

--- base/views.py
+++ base/views.py
@@ -1,4 +1,19 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, FormView
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
 
-def demoText(request):
-    return HttpResponse("Test")
+class CustomLogin(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('posts')
+
+    def form_valid(self, form):
+        # Add success message
+        messages.success(self.request, "Login successful.")
+
+        return super().form_valid(form)

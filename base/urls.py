0bc6be16a57f4e6830321ba30e85c80cef725eca
David-Angel-M <110673903+David-Angel-M@users.noreply.github.com>
2023-06-27 14:37:10 -0500

    urlLinks

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from .views import CustomLogin, Register
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-  path('',views.demoText)
+    path('login', CustomLogin.as_view(), name='login'),
+    path('logout', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register', Register.as_view(), name='register'),
 ]
